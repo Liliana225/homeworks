ee7d3862b95ba85d76fb2540d27eba3c77f0b706
liliana <lili7achatova@gmail.com>
2023-06-06 18:35:14 +0300

    Добавлен вывод на экран суммы, разности, деления и умножения.

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(8+5);
+        System.out.println(27+32);
+        System.out.println(25-8);
+        System.out.println(76/4);
+        System.out.println(5*14);
+
+
+    }
+}
